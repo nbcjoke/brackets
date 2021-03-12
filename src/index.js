5a701d91d7762fbcaf7c2b83d97a10e677bab5fd
Nikita Dymont <gg.joke55@gmail.com>
2021-03-12 16:51:10 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      let beg = bracketsConfig[i][0];
+      let end = bracketsConfig[i][1];
+          for (let j=0; j < str.length; j++) {
+              if (str[j] == beg && str[j+1] ==  end) {
+                  str.splice(j, 2);
+                  i = -1;
+                  j = -1;
+              }
+          }
+  }
+  return !(str.length);
 }
